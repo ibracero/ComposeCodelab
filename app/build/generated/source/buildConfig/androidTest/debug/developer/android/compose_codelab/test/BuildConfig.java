0c7d4d43929b5d35ad8310f249ce2492a7facc26
Imanol Bracero <19324743+ibracero@users.noreply.github.com>
2021-04-29 13:42:03 +0200

    Compose basics google codelab

---
+++ app/build/generated/source/buildConfig/androidTest/debug/developer/android/compose_codelab/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package developer.android.compose_codelab.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "developer.android.compose_codelab.test";
+  public static final String BUILD_TYPE = "debug";
+}
